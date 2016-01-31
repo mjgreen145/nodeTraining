3d86f325426964d7709e26b25880c2edcd0097fc
Matthew Green <mjgreen145@gmail.com>
2016-01-31 18:13:32 +0000

    Finished Express middleware section, started on Handlebars

--- demo/2_Express/routes/routes.js
+++ demo/2_Express/routes/routes.js
@@ -1,21 +1,35 @@
 'use strict';
 
+var fs = require('fs');
+var handlebars = require('handlebars');
+
 var setupRoutes = function(app) {
-	
-	app.get('/', function(req, res) {
-		res.send('Hello World!');
-	});
+  
+  app.get('/', function(req, res) {
+    res.send('Hello World!');
+  });
 
-	app.get('/welcome', function(req, res) {
-		res.send('Welcome!');
-	});
+  app.get('/welcome', function(req, res) {
+    res.send('Welcome!');
+  });
 
-	app.get('/:country/:language/product/:pid', function(req, res) {
-	  res.send(req.params);
-	});
+  app.get('/:country/:language/product/:pid', function(req, res) {
+    res.send(req.params);
+  });
+
+  app.get('/user/:name', function(req, res) {
+    console.log(process.cwd());
+    var template = fs.readFileSync('./demo/2_Express/views/user.hbs').toString();
+
+    var html = handlebars.compile(template)({
+      name: req.params.name
+    });
+
+    res.send(html);
+  });
 
 };
 
 module.exports = {
-	setupRoutes: setupRoutes
+  setupRoutes: setupRoutes
 };
